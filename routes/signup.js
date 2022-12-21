9b304297a6e8a53f4895631af44bc02d560fab41
ebulsok <leebulsok@gmail.com>
2022-12-21 14:41:35 +0900

    feat(signup): 회원가입(아이디/닉네임 중복체크, 비밀번호 암호화)

---
+++ routes/signup.js
@@ -0,0 +1,56 @@
+// @ts-check
+const express = require('express');
+
+const router = express.Router();
+const crypto = require('crypto');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: process.env.SQL_USER,
+  password: process.env.SQL_PW,
+  port: '3306',
+  database: 'alllast',
+  multipleStatements: true,
+});
+
+connection.connect();
+
+const createHashedPassword = (password) => {
+  const salt = crypto.randomBytes(64).toString('base64');
+  const hashedPassword = crypto
+    .pbkdf2Sync(password, salt, 10, 64, 'sha512')
+    .toString('base64');
+  return { hashedPassword, salt };
+};
+
+router.get('/', (req, res) => {
+  res.render('signup');
+});
+
+router.post('/', (req, res) => {
+  const info = req.body;
+  const password = createHashedPassword(info.userPW);
+  connection.query(
+    `INSERT INTO user (user_id, user_name, user_pw, salt) VALUES ('${info.userID}', '${info.userPW}', '${password.hashedPassword}', '${password.salt}')`,
+    (err) => {
+      if (err) throw err;
+      else res.status(201).redirect('/signin');
+    }
+  );
+});
+
+router.post('/check', (req, res) => {
+  const info = req.body;
+  connection.query(
+    `SELECT ${info.category} FROM user WHERE ${info.category} = '${info.data}';`,
+    (err, data) => {
+      if (err) throw err;
+
+      if (data.length === 0) res.json({ result: true });
+      else res.json({ result: false });
+    }
+  );
+});
+
+module.exports = router;
