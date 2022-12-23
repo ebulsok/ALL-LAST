67fa128c5eb4c3ce360e89d73b5cfb8db61c0a23
ebulsok <leebulsok@gmail.com>
2022-12-23 18:55:09 +0900

    feat(home): 홈

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,41 @@
 const express = require('express');
 
 const router = express.Router();
+const mysql = require('mysql');
+const getTotal = require('./log').getTotal;
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: process.env.SQL_USER,
+  password: process.env.SQL_PW,
+  port: '3306',
+  database: 'alllast',
+  multipleStatements: true,
+});
+
+connection.connect();
 
 router.get('/', (req, res) => {
-  res.render('home');
+  connection.query(
+    'SELECT *, DATE_FORMAT(date, "%Y-%m-%d") AS date FROM game WHERE secret = 0 ORDER BY date DESC, place, reg_date DESC;',
+    (err, data) => {
+      if (err) throw err;
+
+      let total = [];
+
+      for (let i = 0; i < data.length; i++) {
+        total.push(
+          getTotal(
+            (data[i].score_1 - 30000) / 1000,
+            (data[i].score_2 - 30000) / 1000,
+            (data[i].score_3 - 30000) / 1000,
+            (data[i].score_4 - 30000) / 1000
+          )
+        );
+      }
+      res.render('home', { data, total });
+    }
+  );
 });
 
 module.exports = router;
