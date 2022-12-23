e5ab694f125c3a298f32993dd9f9cd6b81c3c140
ebulsok <leebulsok@gmail.com>
2022-12-23 12:15:35 +0900

    feat(star): 즐겨찾기 등록, 수정, 삭제

--- routes/log.js
+++ routes/log.js
@@ -42,34 +42,36 @@ function getTotal(s1, s2, s3, s4) {
 }
 
 router.get('/', (req, res) => {
-  if (!req.session.userName) res.redirect('/signin');
+  const name = req.session.userName;
+  if (!name) res.redirect('/signin');
   else {
-    const name = req.session.userName;
-    connection.query(
-      `SELECT *, DATE_FORMAT(date, '%Y/%m/%d') AS date FROM game WHERE player_1 = '${name}' OR player_2 = '${name}' OR player_3 = '${name}' OR player_4 = '${name}';`,
-      (err, data) => {
-        if (err) throw err;
-        else {
-          let total = [];
-          for (let i = 0; i < data.length; i++) {
-            total.push(
-              getTotal(
-                (data[i].score_1 - 30000) / 1000,
-                (data[i].score_2 - 30000) / 1000,
-                (data[i].score_3 - 30000) / 1000,
-                (data[i].score_4 - 30000) / 1000
-              )
-            );
-          }
-          res.status(200).render('log', { data, total });
+    const getGame = `SELECT *, DATE_FORMAT(date, '%Y/%m/%d') AS date FROM game WHERE player_1 = '${name}' OR player_2 = '${name}' OR player_3 = '${name}' OR player_4 = '${name}';`;
+    const getStar = `SELECT game_id FROM star WHERE user_id = '${req.session.userID}';`;
+    connection.query(getGame + getStar, (err, data) => {
+      if (err) throw err;
+      else {
+        const games = data[0];
+        const stars = data[1];
+        let total = [];
+
+        for (let i = 0; i < games.length; i++) {
+          total.push(
+            getTotal(
+              (games[i].score_1 - 30000) / 1000,
+              (games[i].score_2 - 30000) / 1000,
+              (games[i].score_3 - 30000) / 1000,
+              (games[i].score_4 - 30000) / 1000
+            )
+          );
         }
+        res.status(200).render('log', { games, total, stars });
       }
-    );
+    });
   }
 });
 
 router.get('/new', (req, res) => {
-  if (req.session.userID) res.render('record');
+  if (req.session.userID) res.render('newLog');
   else res.redirect('/signin');
 });
 
@@ -79,13 +81,13 @@ router.post('/new', (req, res) => {
   if (d.secret === 'on') secret = 1;
 
   connection.query(
-    `INSERT INTO game (date, place, player_1, player_2, player_3, player_4, score_1, score_2, score_3, score_4, memo, secret) VALUES ('${
+    `INSERT INTO game (date, place, player_1, player_2, player_3, player_4, score_1, score_2, score_3, score_4, secret, reg_date) VALUES ('${
       d.date
     }', '${d.place}', '${d.player1}', '${d.player2}', '${d.player3}', '${
       d.player4
     }', ${Number(d.score1)}, ${Number(d.score2)}, ${Number(d.score3)}, ${Number(
       d.score4
-    )}, '${d.memo}', ${secret});`,
+    )}, ${secret}, now());`,
     (err) => {
       if (err) throw err;
       else res.status(201).redirect('/log');
@@ -102,6 +104,66 @@ router.post('/new/search', (req, res) => {
 
       if (data.length === 0) res.json({ result: false });
       else res.json({ result: true });
+    }
+  );
+});
+
+router.get('/star', (req, res) => {
+  const id = req.session.userID;
+  if (!id) res.redirect('/signin');
+  else {
+    connection.query(
+      `SELECT s.star_id, s.memo, g.*, DATE_FORMAT(g.date, '%Y/%m/%d') AS date FROM star AS s JOIN game AS g ON s.game_id = g.game_id WHERE user_id = '${id}';`,
+      (err, games) => {
+        if (err) throw err;
+        else {
+          let total = [];
+
+          for (let i = 0; i < games.length; i++) {
+            total.push(
+              getTotal(
+                (games[i].score_1 - 30000) / 1000,
+                (games[i].score_2 - 30000) / 1000,
+                (games[i].score_3 - 30000) / 1000,
+                (games[i].score_4 - 30000) / 1000
+              )
+            );
+          }
+          res.status(200).render('star', { games, total });
+        }
+      }
+    );
+  }
+});
+
+router.post('/star', (req, res) => {
+  const info = req.body;
+  connection.query(
+    `INSERT INTO star (game_id, user_id, memo) VALUES (${info.gameID}, '${req.session.userID}', '${info.memo}');`,
+    (err) => {
+      if (err) throw err;
+      else res.status(201).redirect('/log');
+    }
+  );
+});
+
+router.post('/star/edit', (req, res) => {
+  const info = req.body;
+  connection.query(
+    `UPDATE star SET memo = '${info.memo}' WHERE star_id = ${info.starID};`,
+    (err) => {
+      if (err) throw err;
+      else res.status(200).redirect('/log/star');
+    }
+  );
+});
+
+router.delete('/star/:star_id/delete', (req, res) => {
+  connection.query(
+    `DELETE FROM star WHERE star_id = '${req.params.star_id}';`,
+    (err) => {
+      if (err) throw err;
+      else res.status(200).redirect('/log/star');
     }
   );
 });
