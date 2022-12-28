e6c79aa577793a0772bbf8f3315484bac31d071d
ebulsok <leebulsok@gmail.com>
2022-12-28 21:18:24 +0900

    feat(app): router 설정

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 // @ts-check
 const express = require('express');
 
+const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT;
 
 const router = require('./routes/index');
+const signupRouter = require('./routes/signup');
+const signinRouter = require('./routes/signin');
+const logRouter = require('./routes/log');
+const scoreRouter = require('./routes/score');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -15,7 +20,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+app.use(
+  session({
+    secret: 'alllast',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 app.use('/', router);
+app.use('/signup', signupRouter);
+app.use('/signin', signinRouter);
+app.use('/log', logRouter.router);
+app.use('/score', scoreRouter);
 
 app.use((err, req, res, next) => {
   console.log(err.stack);
