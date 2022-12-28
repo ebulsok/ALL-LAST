54438f9a1a0b896fbbe1ef89c7cc2bc553945279
ebulsok <leebulsok@gmail.com>
2022-12-28 21:13:53 +0900

    feat(home, log, score, star): pagination 적용

--- routes/index.js
+++ routes/index.js
@@ -2,41 +2,39 @@
 const express = require('express');
 
 const router = express.Router();
-const mysql = require('mysql');
+
 const getTotal = require('./log').getTotal;
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: process.env.SQL_USER,
-  password: process.env.SQL_PW,
-  port: '3306',
-  database: 'alllast',
-  multipleStatements: true,
-});
-
-connection.connect();
+const connection = require('./dbConnect');
 
 router.get('/', (req, res) => {
-  connection.query(
-    'SELECT *, DATE_FORMAT(date, "%Y-%m-%d") AS date FROM game WHERE secret = 0 ORDER BY date DESC, place, reg_date DESC;',
-    (err, data) => {
-      if (err) throw err;
+  const name = req.session.userName;
+  let page = Number(req.query.page) - 1;
+  if (!req.query.page) page = 1;
 
-      let total = [];
+  const getCnt = 'SELECT COUNT(*) as cnt FROM game WHERE secret = 0;';
+  const getData = `SELECT *, DATE_FORMAT(date, "%Y-%m-%d") AS date FROM game WHERE secret = 0 ORDER BY game_id DESC LIMIT ${
+    page * 5
+  }, 5;`;
+  connection.query(getCnt + getData, (err, result) => {
+    if (err) throw err;
 
-      for (let i = 0; i < data.length; i++) {
-        total.push(
-          getTotal(
-            (data[i].score_1 - 30000) / 1000,
-            (data[i].score_2 - 30000) / 1000,
-            (data[i].score_3 - 30000) / 1000,
-            (data[i].score_4 - 30000) / 1000
-          )
-        );
-      }
-      res.render('home', { data, total });
+    const pageNum = Math.ceil(result[0][0].cnt / 5);
+    const data = result[1];
+    let total = [];
+
+    for (let i = 0; i < data.length; i++) {
+      total.push(
+        getTotal(
+          (data[i].score_1 - 30000) / 1000,
+          (data[i].score_2 - 30000) / 1000,
+          (data[i].score_3 - 30000) / 1000,
+          (data[i].score_4 - 30000) / 1000
+        )
+      );
     }
-  );
+    res.render('home', { data, total, name, pageNum });
+  });
 });
 
 module.exports = router;
--- routes/score.js
+++ routes/score.js
@@ -2,19 +2,9 @@
 const express = require('express');
 
 const router = express.Router();
-const mysql = require('mysql');
 const getTotal = require('./log').getTotal;
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: process.env.SQL_USER,
-  password: process.env.SQL_PW,
-  port: '3306',
-  database: 'alllast',
-  multipleStatements: true,
-});
-
-connection.connect();
+const connection = require('./dbConnect');
 
 // 나의 점수
 router.get('/', (req, res) => {
@@ -22,62 +12,66 @@ router.get('/', (req, res) => {
 
   if (!name) res.redirect('/signin');
   else {
-    connection.query(
-      `SELECT *, DATE_FORMAT(date, '%Y/%m/%d') AS date FROM game WHERE player_1 = '${name}' OR player_2 = '${name}' OR player_3 = '${name}' OR player_4 = '${name}' ORDER BY date DESC, place, reg_date;`,
-      (err, data) => {
-        if (err) throw err;
-        else {
-          let scores = [];
-          let obj = {};
-          for (let i = 0; i < data.length; i++) {
-            if (i === 0)
-              obj = { date: data[i].date, place: data[i].place, games: [] };
-            else if (
-              data[i - 1].date !== data[i].date ||
-              data[i - 1].place !== data[i].place
-            ) {
-              scores.push(obj);
-              obj = { date: data[i].date, place: data[i].place, games: [] };
-            }
+    const page = Number(req.query.page) - 1;
+    const getCnt = `SELECT COUNT(*) as cnt FROM game WHERE player_1 = '${name}' OR player_2 = '${name}' OR player_3 = '${name}' OR player_4 = '${name}';`;
+    const getGame = `SELECT *, DATE_FORMAT(date, '%Y/%m/%d') AS date FROM game WHERE player_1 = '${name}' OR player_2 = '${name}' OR player_3 = '${name}' OR player_4 = '${name}' ORDER BY date DESC, place, reg_date DESC LIMIT ${
+      page * 10
+    }, 10;`;
 
-            let total = getTotal(
-              (data[i].score_1 - 30000) / 1000,
-              (data[i].score_2 - 30000) / 1000,
-              (data[i].score_3 - 30000) / 1000,
-              (data[i].score_4 - 30000) / 1000
-            );
-            if (data[i].player_1 === name) {
-              obj.games.push({
-                rank: 1,
-                score: data[i].score_1,
-                total: total.total_1,
-              });
-            } else if (data[i].player_2 === name) {
-              obj.games.push({
-                rank: 2,
-                score: data[i].score_2,
-                total: total.total_2,
-              });
-            } else if (data[i].player_3 === name) {
-              obj.games.push({
-                rank: 3,
-                score: data[i].score_3,
-                total: total.total_3,
-              });
-            } else if (data[i].player_4 === name) {
-              obj.games.push({
-                rank: 4,
-                score: data[i].score_4,
-                total: total.total_4,
-              });
-
-              if (i === data.length - 1) scores.push(obj);
-            }
+    connection.query(getCnt + getGame, (err, result) => {
+      if (err) throw err;
+      else {
+        const pageNum = Math.ceil(result[0][0].cnt / 10);
+        const data = result[1];
+        let scores = [];
+        let obj = {};
+        for (let i = 0; i < data.length; i++) {
+          if (i === 0)
+            obj = { date: data[i].date, place: data[i].place, games: [] };
+          else if (
+            data[i - 1].date !== data[i].date ||
+            data[i - 1].place !== data[i].place
+          ) {
+            scores.push(obj);
+            obj = { date: data[i].date, place: data[i].place, games: [] };
           }
-          res.status(200).render('score', { scores });
+
+          let total = getTotal(
+            (data[i].score_1 - 30000) / 1000,
+            (data[i].score_2 - 30000) / 1000,
+            (data[i].score_3 - 30000) / 1000,
+            (data[i].score_4 - 30000) / 1000
+          );
+          if (data[i].player_1 === name) {
+            obj.games.push({
+              rank: 1,
+              score: data[i].score_1,
+              total: total.total_1,
+            });
+          } else if (data[i].player_2 === name) {
+            obj.games.push({
+              rank: 2,
+              score: data[i].score_2,
+              total: total.total_2,
+            });
+          } else if (data[i].player_3 === name) {
+            obj.games.push({
+              rank: 3,
+              score: data[i].score_3,
+              total: total.total_3,
+            });
+          } else if (data[i].player_4 === name) {
+            obj.games.push({
+              rank: 4,
+              score: data[i].score_4,
+              total: total.total_4,
+            });
+          }
+          if (i === data.length - 1) scores.push(obj);
         }
+        res.status(200).render('score', { scores, pageNum, name });
       }
-    );
+    });
   }
 });
 
